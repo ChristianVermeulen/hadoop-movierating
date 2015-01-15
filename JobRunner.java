92bb1bebf90fee4bd9c5daea667cdc2bae79af69
Christian Vermeulen <info@christianvermeulen.net>
2015-01-15 14:22:21 +0100

    import project

---
+++ JobRunner.java
@@ -0,0 +1,13 @@
+import org.apache.hadoop.mapred.jobcontrol.JobControl;
+
+public class JobRunner implements Runnable {
+  private JobControl control;
+
+  public JobRunner(JobControl _control) {
+    this.control = _control;
+  }
+
+  public void run() {
+    this.control.run();
+  }
+}
